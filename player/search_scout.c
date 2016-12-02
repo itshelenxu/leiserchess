5029654d382f6e802177589e8da951f2a04b60fa
Quan Li <quanli@mit.edu>
2016-12-02 22:53:50 +0000

    Put important moves in move_list in the front. Coarsen scout_search.

--- player/search_scout.c
+++ player/search_scout.c
@@ -136,6 +136,7 @@ static score_t scout_search(searchNode * node, int depth,
     }
   }
 
+
   if (!cutoff) {
     sort_incremental(move_list + critical_moves, 
                      num_of_moves - critical_moves, num_of_moves - critical_moves);
