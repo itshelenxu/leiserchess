1b50971b6a22b0b3c8ff318e3260b077e14de1cf
Quan Li <quanli@mit.edu>
2016-12-04 04:16:53 +0000

    pprof

--- player/pprof.sh
+++ player/pprof.sh
@@ -1,7 +1,8 @@
 make clean; make PROFILE=1
 ./leiserchess input.txt
 google-pprof --svg leiserchess profile.data > out.svg
-google-pprof --lines --svg leiserchess profile.data > out2.svg
+# google-pprof --lines --svg leiserchess profile.data > out2.svg
+google-pprof --lines leiserchess profile.data > lines.txt
 cp out* ~/www/
 
 # go to http://web.mit.edu/~hjxu/www/out.svg (or out2.svg) to see the results
